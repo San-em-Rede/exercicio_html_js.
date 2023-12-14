6796d26e398a8a0f219ae63c6e791662bd717c69
San-em-Rede <126499089+San-em-Rede@users.noreply.github.com>
2023-12-14 11:27:48 -0400

    1# Mudanças nos codigos

--- main.js
+++ main.js
@@ -1,27 +1,68 @@
-const form = document.querySelector("#form")
-const valueInput = document.querySelector("#valor")
-const balanceInput = document.querySelector("saldo")
+const form = document.getElementById{'form'};
+const campos = document.querySelectorAll('.span-required');
+const spans = document.querySelectorAll('span-required');
+const emailRegex = /^\w+([-+,']\w+)*@\w+([-.]\w+)*\.w+([-.]\w+)+$/;
 
-
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    // Verifica se campo A é menor que B
-    if ((valueInput ==< balanceInput) = true) {
-        alert("Você possui saldo o bastante")
-        return;
-    } else ((valueInput ==< balanceInput) = false) { 
-        alert("Você não possui saldo o bastante")
+    nameValidate();
+    emailValidate();
+    mainPasswordValidate();
+    comparePassword();
+
+
+});
+
+function setError(index){
+    alert(index).style.border = '2px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function removeError(index){
+    campos[index].style.border = '';
+    campos[index].style.display = 'nome';
+}
+
+function nameValidate(){
+    if(campos [0].value.length < 3)
+    {
+        setError(0);
     }
-})
+    else
+    {
+        removeError(0);
+}
 
-/*obs: 
+function emailValidadete(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else
+    {
+        removeError(1);
+    }
+}
 
-1.No meu VS Code, as variaveis e funções não estão com as cores corretas
-o que pode ser? 
+function mainPasswordValidate(){
+    if(campos[2].value.lenght < 8)
+    {
+        setError(2);
+    }
+    else
+    {
+        removeError(2);
+        comparePassword();
+    }
+}
 
-2. Tem como me repassar todas as extensões do VS code utilizadas no curso
-da EBAC?
-
-3. Estou com dificuldade nesse exercicio
-
-*/
+function comparePassword(){
+    if(campos[2].value == campos[3].value && campos[3].value.lenght >= 8)
+    {
+        removeError(3);
+    }
+    else
+    {
+        setError(3);
+    }
+}
